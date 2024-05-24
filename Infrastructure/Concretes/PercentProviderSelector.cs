2a31c82de3b8819f1aefd65d507a1b385324a7cd
beka maisuradze <beka.maisuradze@singular.uk>
2024-05-24 16:56:30 +0400

    remove comment

--- Infrastructure/Concretes/PercentProviderSelector.cs
+++ Infrastructure/Concretes/PercentProviderSelector.cs
@@ -1,35 +1,35 @@
-﻿//using Application.Abstracts;
+﻿using Application.Abstracts;
 
-//namespace Infrastructure.Concretes;
+namespace Infrastructure.Concretes;
 
-//public class PercentProviderSelector : IProviderSelector
-//{
-//    private readonly List<ISmsProvider> _smsProviders;
-//    private readonly Dictionary<ISmsProvider, int> _providerWeights;
-//    private readonly Random _random;
+public class PercentProviderSelector : IProviderSelector
+{
+    private readonly List<ISmsProvider> _smsProviders;
+    private readonly Dictionary<ISmsProvider, int> _providerWeights;
+    private readonly Random _random;
 
-//    public PercentProviderSelector(IEnumerable<ISmsProvider> smsProviders, Dictionary<ISmsProvider, int> providerWeights)
-//    {
-//        _smsProviders = smsProviders.ToList();
-//        _providerWeights = providerWeights;
-//        _random = new Random();
-//    }
+    public PercentProviderSelector(IEnumerable<ISmsProvider> smsProviders, Dictionary<ISmsProvider, int> providerWeights)
+    {
+        _smsProviders = smsProviders.ToList();
+        _providerWeights = providerWeights;
+        _random = new Random();
+    }
 
-//    public ISmsProvider SelectProvider()
-//    {
-//        int totalWeight = _providerWeights.Values.Sum();
-//        int randomValue = _random.Next(totalWeight);
-//        int cumulativeWeight = 0;
+    public ISmsProvider SelectProvider()
+    {
+        int totalWeight = _providerWeights.Values.Sum();
+        int randomValue = _random.Next(totalWeight);
+        int cumulativeWeight = 0;
 
-//        foreach (var provider in _providerWeights)
-//        {
-//            cumulativeWeight += provider.Value;
-//            if (randomValue < cumulativeWeight)
-//            {
-//                return provider.Key;
-//            }
-//        }
+        foreach (var provider in _providerWeights)
+        {
+            cumulativeWeight += provider.Value;
+            if (randomValue < cumulativeWeight)
+            {
+                return provider.Key;
+            }
+        }
 
-//        return _smsProviders.Last();
-//    }
-//}
+        return _smsProviders.Last();
+    }
+}
